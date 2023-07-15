30f5571664f892ae0130f5e3ab74b8484cc275d3
Mr.VACO <brute1904@gmail.com>
2023-07-15 16:46:29 +0500

    Added field width parameter

--- src/AssetServiceProvider.php
+++ src/AssetServiceProvider.php
@@ -4,6 +4,7 @@ namespace MrVaco\NovaPanels;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Nova;
 
 class AssetServiceProvider extends ServiceProvider
@@ -14,6 +15,17 @@ class AssetServiceProvider extends ServiceProvider
         {
             Nova::script('nova-panels', __DIR__ . '/../dist/js/asset.js');
             Nova::style('nova-panels', __DIR__ . '/../dist/css/asset.css');
+            
+            /*
+             * Comes with a 12 point grid system
+             * width-1/12 to width-12/12
+             */
+            Field::macro('width', function(int $columns = 12)
+            {
+                return $this->withMeta([
+                    'width' => $columns
+                ]);
+            });
         });
     }
 }
